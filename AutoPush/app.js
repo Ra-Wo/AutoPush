6ee66b8f6bcb79f604bb66dee053e5daab508e01
Rachid oudouch <rashidoudouch@gmail.com>
2022-02-18 16:33:49 +0100

    update

--- AutoPush/app.js
+++ AutoPush/app.js
@@ -13,23 +13,8 @@ function push() {
 	readline.question("\033[1;33m╭─AutoPush\n╰─$ \033[0m", msg => {
 		if (msg == ".auto")
 		{
-			setInterval(() => {
-				exec('clear; cd .. ; git add . ; git commit -m "update" ; git push',
-					function (errorcmd, stdout, stderr) {
-						if (stdout && message != stdout)
-						{
-							console.log(stdout);
-							message = stdout;
-						}
-						if (stderr && error != stderr)
-						{
-							console.log('\033[0;32m' + stderr + "\033[0m");
-							error = stderr;
-						}
-						if (errorcmd !== null)
-							 console.log('\033[1;31m exec error: ' + errorcmd + "\033[0m");
-					});
-			}, 3000);
+			push_to_github();
+			setInterval(push_to_github, 3000);
 		}
 		console.log("\n");
 		exec(`cd .. ; git add . ; git commit -m "${msg}" ; git push`,
@@ -47,9 +32,26 @@ function push() {
 	})
 }
 
-app.listen(port, (err) => {
-	if (err) console.log(err)
-	console.log("\033[0;32m Starting...\n\033[0m")
+function push_to_github() {
+	exec('clear; cd .. ; git add . ; git commit -m "update" ; git push',
+		function (errorcmd, stdout, stderr) {
+			if (stdout && message != stdout)
+			{
+				console.log(stdout);
+				message = stdout;
+			}
+			if (stderr && error != stderr)
+			{
+				console.log('\033[0;32m' + stderr + "\033[0m");
+				error = stderr;
+			}
+			if (errorcmd !== null)
+				 console.log('\033[1;31m exec error: ' + errorcmd + "\033[0m");
+		}
+	);
+}
+
+function clear() {
 	exec(`clear`,
 		function (errorcmd, stdout, stderr) {
 			if (stdout)
@@ -62,5 +64,11 @@ app.listen(port, (err) => {
 			push()
 		}
 	);
+}
+
+app.listen(port, (err) => {
+	if (err) console.log(err)
+	console.log("\033[0;32m Starting...\n\033[0m")
+	clear();
 	push();
 })
